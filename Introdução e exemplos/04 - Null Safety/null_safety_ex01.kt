49510f9221b878c8b140d3aacb4dc2beadf76896
Antonio Santos <86670111+tonicosud@users.noreply.github.com>
2023-03-22 16:09:15 -0300

    Update null_safety_ex01.kt

--- Introdução e exemplos/04 - Null Safety/null_safety_ex01.kt
+++ Introdução e exemplos/04 - Null Safety/null_safety_ex01.kt
@@ -1,19 +1,23 @@
 /**
- * Classes Tradicionais.
+ * Nulidade no Kotlin.
 */
-
-class Customer                                  // 1
-
-class Contact(val id: Int, var email: String)   // 2
 
 fun main() {
 
-    val customer = Customer()                   // 3
+    var neverNull: String = "This can't be null"            // 1
+    //neverNull = null                                      // 2
+    
+    var nullable: String? = "You can keep a null here"      // 3
+    nullable = null                                         // 4
+    
+    var inferredNonNull = "The compiler assumes non-null"   // 5
+    //inferredNonNull = null                                // 6
+    
+    fun strLength(str: String?): Int {                      // 7
+        return str?.length ?: 0
+    }
+    
+    println(strLength(neverNull))                           // 8
+    println(strLength(nullable))                            // 9
 
-    val contact = Contact(1, "mary@gmail.com")  // 4
-
-    println(contact.id)                         // 5
-    println(contact.email)
-    contact.email = "jane@gmail.com"            // 6
-    println(contact.email)
 }
