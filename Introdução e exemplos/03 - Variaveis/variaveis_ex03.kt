8ff53e1c827f4737ec4ebe8caa065a3a1188d09b
Antonio Santos <86670111+tonicosud@users.noreply.github.com>
2023-03-22 16:07:43 -0300

    Update variaveis_ex03.kt

--- Introdução e exemplos/03 - Variaveis/variaveis_ex03.kt
+++ Introdução e exemplos/03 - Variaveis/variaveis_ex03.kt
@@ -1,23 +1,19 @@
 /**
- * Nulidade no Kotlin.
-*/
+ * Variáveis: [var] e [val].
+ */
+
+fun someCondition() = true 
 
 fun main() {
 
-    var neverNull: String = "This can't be null"            // 1
-    //neverNull = null                                      // 2
-
-    var nullable: String? = "You can keep a null here"      // 3
-    nullable = null                                         // 4
-
-    var inferredNonNull = "The compiler assumes non-null"   // 5
-    //inferredNonNull = null                                // 6
-
-    fun strLength(str: String?): Int {                      // 7
-        return str?.length ?: 0
+    val d: Int  // 1
+    
+    if (someCondition()) {
+        d = 1   // 2
+    } else {
+        d = 2   // 2
     }
-
-    println(strLength(neverNull))                           // 8
-    println(strLength(nullable))                            // 9
+    
+    println(d) // 3
 
 }
