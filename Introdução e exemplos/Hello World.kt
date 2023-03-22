baafd484173421365caff6d70792c2c2cd25c3e9
Antonio Santos <86670111+tonicosud@users.noreply.github.com>
2023-03-22 14:57:49 -0300

    Exemplo Hello World

---
+++ Introdução e exemplos/Hello World.kt
@@ -0,0 +1,10 @@
+/**
+ * Hello, World!
+ *
+ **/
+
+package me.dio.helloworld          // 1. Definição do pacote (opcional).
+
+fun main() {                       // 2. Ponto de entrada de um código Kotlin, definido pela função [main].
+    println("Hello, World!")       // 3. Imprime na saída padrão um texto usando a função [println]
+}
