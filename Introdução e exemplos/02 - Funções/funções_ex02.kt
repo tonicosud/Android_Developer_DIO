03c88942e4cb83dcf9407f458637c192f7e184f8
Antonio Santos <86670111+tonicosud@users.noreply.github.com>
2023-03-22 15:44:03 -0300

    Update funções_ex02.kt

--- Introdução e exemplos/02 - Funções/funções_ex02.kt
+++ Introdução e exemplos/02 - Funções/funções_ex02.kt
@@ -1,11 +1,25 @@
 /**
- * Variáveis: [var] e [val].
-*/
+ * Funções com parâmetros [vararg].
+ *
+ * @see [Functions](https://play.kotlinlang.org/byExample/01_introduction/02_Functions)
+ */
 fun main() {
 
-    var a: String = "initial"  // 1
-    println(a)
-    val b: Int = 1             // 2
-    val c = 3                  // 3
+    fun printAll(vararg messages: String) {                            // 1
+        for (m in messages) println(m)
+    }
+    printAll("Hello", "Olá", "Salut", "Hola", "你好")                  // 2
+    
+    fun printAllWithPrefix(vararg messages: String, prefix: String) {  // 3
+        for (m in messages) println(prefix + m)
+    }
+    printAllWithPrefix(
+            "Hello", "Olá", "Salut", "Hola", "你好",
+            prefix = "Greeting: "                                      // 4
+    )
+
+    fun log(vararg entries: String) {
+        printAll(*entries)                                             // 5
+    }
 
 }
