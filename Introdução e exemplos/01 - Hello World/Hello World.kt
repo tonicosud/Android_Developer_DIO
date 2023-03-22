87cf5a7fe140846ef42330600a864929dd41edf4
Antonio Santos <86670111+tonicosud@users.noreply.github.com>
2023-03-22 15:56:15 -0300

    update HelloWorld

--- Introdução e exemplos/01 - Hello World/Hello World.kt
+++ Introdução e exemplos/01 - Hello World/Hello World.kt
@@ -1,26 +1,9 @@
 /**
- * Valores de parâmetro padrão e argumentos nomeados.
-*/
+ * Hello, World!
+ */
 
-fun printMessage(message: String): Unit {                               // 1
-    println(message)
-}
+package me.mypackage.helloworld           // 1. Definição do pacote (opcional).
 
-fun printMessageWithPrefix(message: String, prefix: String = "Info") {  // 2
-    println("[$prefix] $message")
-}
-
-fun sum(x: Int, y: Int): Int {                                          // 3
-    return x + y
-}
-
-fun multiply(x: Int, y: Int) = x * y                                    // 4
-
-fun main() {
-    printMessage("Hello")                                               // 5
-    printMessageWithPrefix("Hello", "Log")                              // 6
-    printMessageWithPrefix("Hello")                                     // 7
-    printMessageWithPrefix(prefix = "Log", message = "Hello")           // 8
-    println(sum(1, 2))                                                  // 9
-    println(multiply(2, 4))                                             // 10
+fun main() {                        // 2. Ponto de entrada de um código Kotlin, definido pela função [main].
+    println("Hello, World!")        // 3. Imprime na saída padrão um texto usando a função [println]
 }
