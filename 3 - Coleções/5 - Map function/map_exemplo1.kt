c3f79b6f10420717801a1999c7b33f7dea50df94
Antonio Santos <86670111+tonicosud@users.noreply.github.com>
2023-03-24 07:11:55 -0300

    Exemplos base de Coleções.

---
+++ 3 - Coleções/5 - Map function/map_exemplo1.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val numbers = listOf(1, -2, 3, -4, 5, -6)     // 1
+    
+    val doubled = numbers.map { x -> x * 2 }      // 2
+    
+    val tripled = numbers.map { it * 3 }          // 3
+
+    println("Numbers: $numbers")
+    println("Doubled Numbers: $doubled")
+    println("Tripled Numbers: $tripled")
+}
