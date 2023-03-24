498ea57366de5ccc5ea28aeea08d6877222f5582
Antonio Santos <86670111+tonicosud@users.noreply.github.com>
2023-03-24 07:11:36 -0300

    Exemplos de controle de fluxo

---
+++ 2 - controle de fluxo/2 - loops/loops_exemplo1.kt
@@ -0,0 +1,12 @@
+/**
+ * Estrutura de repetição/loop [for].
+*/
+fun main() {
+
+    val cakes = listOf("carrot", "cheese", "chocolate")
+
+    for (cake in cakes) {                               // 1
+        println("Yummy, it's a $cake cake!")
+    }
+
+}
---
+++ 2 - controle de fluxo/2 - loops/loops_exemplo3.kt
@@ -0,0 +1,23 @@
+/**
+ * Iterators.
+*/
+
+class Animal(val name: String)
+
+class Zoo(val animals: List<Animal>) {
+
+    operator fun iterator(): Iterator<Animal> {             // 1
+        return animals.iterator()                           // 2
+    }
+}
+
+fun main() {
+
+    val animals = listOf(Animal("zebra"), Animal("lion"));
+    val zoo = Zoo(animals)
+
+    for (animal in zoo) {                                   // 3
+        println("Watch out, it's a ${animal.name}")
+    }
+
+}
