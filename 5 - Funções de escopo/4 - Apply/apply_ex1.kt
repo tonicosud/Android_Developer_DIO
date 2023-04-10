fca8627a6a93e51cfdf36fa577994ccda5598ec9
Antonio Santos <86670111+tonicosud@users.noreply.github.com>
2023-04-10 07:46:17 -0300

    Códigos base para Funções de Escopo e Funções Avançadas

---
+++ 5 - Funções de escopo/4 - Apply/apply_ex1.kt
@@ -0,0 +1,19 @@
+/**
+ * Funções de Escopo: [apply].
+*/
+
+data class Person(var name: String, var age: Int, var about: String) {
+    constructor() : this("", 0, "")
+}
+
+fun main() {
+
+    val jake = Person()                                     // 1
+    val stringDescription = jake.apply {                    // 2
+        name = "Jake"                                       // 3
+        age = 30
+        about = "Android developer"
+    }.toString()                                            // 4
+
+    println(stringDescription)
+}
