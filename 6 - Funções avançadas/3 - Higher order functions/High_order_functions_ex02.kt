b6fba253973932715bb21e2228e708018dd02899
Antonio Santos <86670111+tonicosud@users.noreply.github.com>
2023-04-11 07:19:56 -0300

    Ajuste no nome do pacote de Funções Avançadas

---
+++ 6 - Funções avançadas/3 - Higher order functions/High_order_functions_ex02.kt
@@ -0,0 +1,14 @@
+/**
+ * Retornando funções.
+*/
+
+fun operation(): (Int) -> Int {                                     // 1
+    return ::square
+}
+
+fun square(x: Int) = x * x                                          // 2
+
+fun main() {
+    val func = operation()                                          // 3
+    println(func(2))                                                // 4
+}
